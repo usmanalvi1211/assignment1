5e7d751a0d2de604152fd0a35a2b1203eb5c8268
usmanalvi1211 <112401166+usmanalvi1211@users.noreply.github.com>
2022-10-04 15:05:23 -1200

    practice for factorial with loops and recursion

--- ass1.js
+++ ass1.js
@@ -1,11 +1,13 @@
-let year = parseInt(prompt("Enter the year for checking its a leap or not"))
+// let year = parseInt(prompt("Enter the year for checking its a leap or not"))
 
-    if(year == 0){
-    console.log("Try again")
-    }
-    else if (year%4==0){
-        console.log(`Yes its a leap year: ${year}`);
-    }
-    else{
-        console.log("no its not a leap year");
-    }
+//     if(year == 0){
+//     console.log("Try again")
+//     }
+//     else if (year%4==0){
+//         console.log(`Yes its a leap year: ${year}`);
+//     }
+//     else{
+//         console.log("no its not a leap year");
+//     }
+
+
